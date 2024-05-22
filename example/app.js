f75666bde97388620d6558ce454e7095090201d6
Vaughn Dabney <vaughn@emptyboxmedia.com>
2024-05-22 14:31:47 -0700

    updated 1.2.0
    - added examples

--- example/app.js
+++ example/app.js
@@ -3,34 +3,96 @@
 // to test out the module and to provide instructions
 // to users on how to use it by example.
 
-
+var shareImage = require('com.ebm.shareimage');
 // open a single window
-const win = Ti.UI.createWindow();
-const label = Ti.UI.createLabel();
-win.add(label);
-win.open();
+var win = Ti.UI.createWindow({
+	layout:'vertical'
+});
 
-// TODO: write your module tests here
-import shareimage  from 'com.ebm.shareimage';
-Ti.API.info("module is => " + shareimage);
 
-label.text = shareimage.example();
+var myImg = Ti.UI.createImageView({
+	image:'yourimage.png',
+	defaultImage:null,
+	width:500,
+	height:500,
+	top:100
+});
 
-Ti.API.info("module exampleProp is => " + shareimage.exampleProp);
-shareimage.exampleProp = "This is a test value";
 
-if (Ti.Platform.name == "android") {
-	const proxy = shareimage.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
+var myLbl = Ti.UI.createLabel({
+	text:'This is my label',
+	width:Ti.UI.SIZE,
+	height:Ti.UI.SIZE,
+	top:50,
+	color:'#000'
+});
+
+//image share button
+var btn1 = Ti.UI.createButton({
+	title:'share image',
+	top:10
+});
+	btn1.addEventListener('click',function(){
+		//image share
+		shareImage.share({
+			image: myImg,
+			callback: function(e) {
+				if (e.success) {
+					Ti.UI.createAlertDialog({ message: 'Shared successfully!' }).show();
+				} else {
+					Ti.UI.createAlertDialog({ message: 'Failed to share.' }).show();
+				}
+			}
+		});
 	});
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
+
+//text share button
+var btn2 = Ti.UI.createButton({
+	title:'share text',
+	top:10
+});
+
+	btn2.addEventListener('click',function(){
+		//text share
+		shareImage.share({
+			text: myLbl.text,
+			callback: function(e) {
+				if (e.success) {
+					Ti.UI.createAlertDialog({ message: 'Shared successfully!' }).show();
+				} else {
+					Ti.UI.createAlertDialog({ message: 'Failed to share.' }).show();
+				}
+			}
+		});
+	});
+
+//text and image share button
+	var btn3 = Ti.UI.createButton({
+		title:'share text and image',
+		top:10
+	});
+	
+		btn3.addEventListener('click',function(){
+			//text share
+			shareImage.share({
+				text: myLbl.text,
+				image: myImg,
+				callback: function(e) {
+					if (e.success) {
+						Ti.UI.createAlertDialog({ message: 'Shared successfully!' }).show();
+					} else {
+						Ti.UI.createAlertDialog({ message: 'Failed to share.' }).show();
+					}
+				}
+			});
+		});
+
+
+win.add(myImg);
+win.add(myLbl);
+win.add(btn1);
+//win.add(btn2);
+//win.add(btn3);
+
+win.open();
